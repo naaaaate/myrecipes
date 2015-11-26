d20b8d3c4db9644344210504faf3a8d3f2bce12d
Nathan Lee <nlee43@gmail.com>
2015-11-25 20:36:11 -0800

    create chefs and recipes associations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,8 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
+  before_save {self.email = email.downcase }
+  validates :chefname, presence: true, length: {minimum:3, maximum:40}
+  validates :email, presence: true, length: {maximum: 105},
+                                    uniqueness: {case_sensitive: false}
 
 end
